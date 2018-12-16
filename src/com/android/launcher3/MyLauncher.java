16c65434b5487a584e6bbad606b915dff1a74575
Fan Day <dayfan0810@gmail.com>
2018-12-17 00:43:46 +0800

    负一屏02

---
+++ src/com/android/launcher3/MyLauncher.java
@@ -0,0 +1,10 @@
+package com.android.launcher3;
+
+import android.os.Bundle;
+
+public class MyLauncher extends LauncherExtension {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
